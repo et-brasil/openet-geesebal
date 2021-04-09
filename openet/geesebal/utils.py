6f861e60053d4077a8cfad1867959b03e0d18079
Charles Morton <charles.g.morton@gmail.com>
2021-04-09 10:54:40 -0700

    Initial testing support on utils

--- openet/geesebal/utils.py
+++ openet/geesebal/utils.py
@@ -30,26 +30,34 @@ def getinfo(ee_obj, n=4):
     # output = ee_obj.getInfo()
     return output
 
+
 def c_to_k(image):
     """Convert temperature image from C to K
+
     Parameters
     ----------
     image : ee.Image
+
     Returns
     -------
     ee.Image
     """
+
     return image.add(273.15)
+
 
 def date_to_time_0utc(date):
     """Get the 0 UTC time_start for a date
+
     Parameters
     ----------
     date : ee.Date
+
     Returns
     -------
     ee.Number
     """
+
     return ee.Date.fromYMD(date.get('year'), date.get('month'),
                            date.get('day')).millis()
     # Extra operations are needed since update() does not set milliseconds to 0.
@@ -62,6 +70,7 @@ def date_to_time_0utc(date):
 # I'm not sure how to make them fixtures and allow input parameters
 def constant_image_value(image, crs='EPSG:32613', scale=1):
     """Extract the output value from a calculation done with constant images"""
+
     return getinfo(ee.Image(image).reduceRegion(
         reducer=ee.Reducer.first(), scale=scale,
         geometry=ee.Geometry.Rectangle([0, 0, 10, 10], crs, False)))
@@ -69,6 +78,7 @@ def constant_image_value(image, crs='EPSG:32613', scale=1):
 
 def point_image_value(image, xy, scale=1):
     """Extract the output value from a calculation at a point"""
+
     return getinfo(ee.Image(image).reduceRegion(
         reducer=ee.Reducer.first(), geometry=ee.Geometry.Point(xy),
         scale=scale))
@@ -90,6 +100,7 @@ def point_coll_value(coll, xy, scale=1):
             '%Y-%m-%d')
         for k, v in col_dict.items():
             info_dict[k][date] = row[col_dict[k]]
+
     return info_dict
     # return pd.DataFrame.from_dict(info_dict)
 
@@ -104,13 +115,16 @@ def is_number(x):
 
 def millis(input_dt):
     """Convert datetime to milliseconds since epoch
+
     Parameters
     ----------
     input_dt : datetime
+
     Returns
     -------
     int
     """
+
     return 1000 * int(calendar.timegm(input_dt.timetuple()))
 
 
