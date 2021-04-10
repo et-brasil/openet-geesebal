2f1fc3ae9dade2d2ef2597466b3e35cd9d4b8fe4
Charles Morton <charles.g.morton@gmail.com>
2021-04-10 07:36:25 -0700

    Started an initial test file for the model.py

---
+++ openet/geesebal/tests/test_c_model.py
@@ -0,0 +1,21 @@
+import ee
+import pytest
+
+import openet.geesebal.model as model
+import openet.geesebal.utils as utils
+# TODO: import utils from openet.core
+# import openet.core.utils as utils
+
+
+@pytest.mark.parametrize(
+    'rn, ndvi, albedo, lst_dem, expected',
+    [
+        [100, 0.5, 0.2, 300, 13.3],
+    ]
+)
+def test_soil_heat_flux(rn, ndvi, albedo, lst_dem, expected, tol=0.1):
+    output = utils.constant_image_value(model.soil_heat_flux(
+        rn=ee.Image.constant(rn), ndvi=ee.Image.constant(ndvi),
+        albedo=ee.Image.constant(albedo),
+        lst_dem=ee.Image.constant(lst_dem)))
+    assert abs(output - expected) <= tol
