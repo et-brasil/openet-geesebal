f98c8e82f8dda858876afb3281e601dfb8deb387
leolaipelt <leolaipelt@hotmail.com>
2021-05-12 14:23:42 -0300

    Bumped version number.

--- openet/geesebal/__init__.py
+++ openet/geesebal/__init__.py
@@ -2,6 +2,6 @@ from .openet_image import Image
 from .openet_collection import Collection
 from . import openet_interpolate as interpolate
 
-__version__ = '0.1.9'
+__version__ = '0.2.0'
 
 MODEL_NAME = 'GEESEBAL'
