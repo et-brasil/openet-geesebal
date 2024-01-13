c726b4839574e3ea2d8cf62ff1a3cf60fc31bad2
Charles Morton <cgmorton@users.noreply.github.com>
2024-01-12 16:36:17 -0800

    Bumping patch version number

--- openet/geesebal/__init__.py
+++ openet/geesebal/__init__.py
@@ -2,6 +2,6 @@ from .openet_image import Image
 from .openet_collection import Collection
 from . import openet_interpolate as interpolate
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 MODEL_NAME = 'GEESEBAL'
